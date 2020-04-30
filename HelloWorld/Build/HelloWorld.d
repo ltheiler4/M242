f119f9f84b7f61d38a49aa506d0cd52dda157e0b
Lars Theiler <theilel4@emea.roche.com>
2020-04-30 08:31:39 +0200

    hello world files

---
+++ HelloWorld/Build/HelloWorld.d
@@ -0,0 +1,51 @@
+C:\M242\prj\HelloWorld\Build\HelloWorld.d \
+ C:\M242\prj\HelloWorld\Build\HelloWorld.o: \
+ C:\M242\prj\HelloWorld\Build\HelloWorld.c \
+ c:\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\io.h \
+ c:\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\sfr_defs.h \
+ c:\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\inttypes.h \
+ c:\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\lib\gcc\avr\5.4.0\include\stdint.h \
+ c:\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\stdint.h \
+ c:\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\iom328p.h \
+ c:\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\portpins.h \
+ c:\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\common.h \
+ c:\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\version.h \
+ c:\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\fuse.h \
+ c:\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\lock.h \
+ C:\M242\avr\ToolChain\AvrLib\include/Atmega328P.h \
+ C:\M242\avr\ToolChain\AvrLib\include/avrlib.h \
+ c:\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\util\delay.h \
+ c:\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\util\delay_basic.h \
+ c:\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\math.h
+
+c:\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\io.h:
+
+c:\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\sfr_defs.h:
+
+c:\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\inttypes.h:
+
+c:\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\lib\gcc\avr\5.4.0\include\stdint.h:
+
+c:\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\stdint.h:
+
+c:\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\iom328p.h:
+
+c:\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\portpins.h:
+
+c:\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\common.h:
+
+c:\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\version.h:
+
+c:\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\fuse.h:
+
+c:\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\lock.h:
+
+C:\M242\avr\ToolChain\AvrLib\include/Atmega328P.h:
+
+C:\M242\avr\ToolChain\AvrLib\include/avrlib.h:
+
+c:\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\util\delay.h:
+
+c:\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\util\delay_basic.h:
+
+c:\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\math.h:
---
+++ HelloWorld/HelloWorld.c
@@ -0,0 +1,18 @@
+#include <avr\io.h>
+#include <Atmega328P.h>
+#include <avrlib.h>
+#define F_CPU 16000000
+#include <util\delay.h>
+#include <inttypes.h>
+
+int main(void)
+{
+	Usart_Init(250000);
+	
+	while (True)
+	{
+		TRACE("Hello World !");
+		_delay_ms(1000);
+	}
+	return 0;
+}
